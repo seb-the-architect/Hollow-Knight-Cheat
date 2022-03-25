1f8c0a9d555456b8e68f617062d46bd996705ff2
seb-the-architect <102254762+seb-the-architect@users.noreply.github.com>
2022-03-25 00:25:10 +0000

    Add files via upload

---
+++ HollowCheat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+class Main : MonoBehaviour
+{
+    bool bMenuVisible = false;
+    bool bSoulfull = false;
+    bool bInvincible = false;
+
+    int DamageSliderValue = 0;
+
+    public void OnUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Insert)) // Will just unload our DLL
+        {
+            bMenuVisible = !bMenuVisible;
+        }
+        if (bSoulfull)
+        {
+            m_PlayerData.MPCharge = m_PlayerData.maxMP;
+        }
+        if (bInvincible)
+        {
+            m_PlayerData.health = m_PlayerData.maxHealth;
+        }
+        m_PlayerData = PlayerData.instance;
+    }
+
+    public void OnGUI()
+    {
+        if (bMenuVisible)
+        {
+            GUI.Box(new Rect(Screen.width-250, 10, 200, 180), "Seb's Hollow Knight Hack");
+
+            bSoulfull = GUI.Toggle(new Rect(Screen.width - 250, 40, 160, 20), bSoulfull, "Infinite Soul");
+
+            DamageSliderValue = (int)GUI.HorizontalSlider(new Rect(Screen.width - 250, 70, 100, 30), DamageSliderValue, 0.0F, 500.0F);
+
+            GUI.Label(new Rect(Screen.width - 140, 90, 100, 20), DamageSliderValue.ToString());
+
+            if (GUI.Button(new Rect(Screen.width - 140, 70, 82, 22), "Set Damage"))
+            {
+                m_PlayerData.nailDamage = DamageSliderValue;
+            }
+
+            bInvincible = GUI.Toggle(new Rect(Screen.width - 250, 100, 160, 20), bInvincible, "Invincible");
+
+            if (GUI.Button(new Rect(Screen.width - 250, 130, 82, 20), "Add 50 Geo"))
+            {
+                m_PlayerData.geo += 50;
+            }
+        }
+    }
+
+    PlayerData m_PlayerData;
+}
+
